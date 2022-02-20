88002c7e4cd83f67512cc82bc860b232f65b505b
Aspirus <99119682+xAspirus@users.noreply.github.com>
2022-02-21 00:34:07 +0530

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -4,3 +4,13 @@ set -e
 
 cp dnf.conf /etc/dnf/dnf.conf
 dnf upgrade
+
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf group -y update core
+dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+dnf install -y lame\* --exclude=lame-devel
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+dnf group -y upgrade --with-optional Multimedia
+
+dnf install -y tilix micro geany gnome-tweaks gnome-extensions-app
